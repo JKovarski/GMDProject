28f57dc861a683adf133ce1fd77d2ef797b3c6f4
Pierre Kovarski <pierre.kovarski@telecomnancy.eu>
2018-05-23 16:24:15 +0200

    merge

--- esearchQuery.py
+++ esearchQuery.py
@@ -5,57 +5,62 @@ es = Elasticsearch()
 INDEX_NAME = "syn"
 INDEX_NAME2 = "omimtxt"
 INDEX_NAME3 = "orphadata"
-symptom1 = "cervical"
-symptom2 = "neck"
+# symptom1 = "cervical"
+# symptom2 = "neck"
+#
+# def getORPHAandOMIMdiseasesfromSymptom(symptom1,symptom2):
+#     res=es.search(index=INDEX_NAME,body={
+#
+#         "query": {
+#
+#             "bool": {
+#
+#                 "must": {
+#
+#                  "wildcard": { "symptoms": symptom1 },
+#
+#                 },
+#
+#                 "must": {
+#
+#                     "wildcard": { "symptoms": symptom1}
+#                 }
+#
+#             }
+#
+#         }
+#
+#     })
+#
+#     print("results:")
+#
+#     diseases = []
+#     for hit in res['hits']['hits']:
+#         #print(hit["_source"])
+#         omim = hit["_source"]["OMIMid"]
+#         orpha = hit["_source"]["ORPHAid"]
+#         for omimid in omim :
+#             resOMIM = es.search(index=INDEX_NAME2,body={"query":{'match':{'OMIMid':omimid}}})
+#             for hit in resOMIM["hits"]["hits"]:
+#                 diseases.append(hit["_source"]["OMIMdiseases"])
+#                 # print("omim :")
+#                 # print(hit["_source"]["OMIMid"])
+#                 # print(hit["_source"]["OMIMdiseases"])
+#         for orphaid in orpha : #my index has doublons so i take only the first result
+#             resORPHA = es.search(index=INDEX_NAME3,body={"size":1,"query":{'match':{'ORPHAid':orphaid.split(":")[1]}}})
+#             for hit in resORPHA["hits"]["hits"]:
+#                 diseases.append(hit["_source"]["diseases"])
+#                 # print("orpha :")
+#                 # print(hit["_source"]["ORPHAid"])
+#                 # print(hit["_source"]["diseases"])
+#     return diseases
+# #def createQueryEsearch(listSymptom)
+#
+# output = getORPHAandOMIMdiseasesfromSymptom(symptom1,symptom2)
+# print(output)
+# print(len(output))
 
-def getORPHAandOMIMdiseasesfromSymptom(symptom1,symptom2):
-    res=es.search(index=INDEX_NAME,body={
-
-        "query": {
-
-            "bool": {
-
-                "must": {
-
-                 "wildcard": { "symptoms": symptom1 },
-
-                },
-
-                "must": {
-
-                    "match": { "symptoms": symptom1}
-                }
-
-            }
-
-        }
-
-    })
-
-    print("results:")
-
-    diseases = []
-    for hit in res['hits']['hits']:
-        #print(hit["_source"])
-        omim = hit["_source"]["OMIMid"]
-        orpha = hit["_source"]["ORPHAid"]
-        for omimid in omim :
-            resOMIM = es.search(index=INDEX_NAME2,body={"query":{'match':{'OMIMid':omimid}}})
-            for hit in resOMIM["hits"]["hits"]:
-                diseases.append(hit["_source"]["OMIMdiseases"])
-                # print("omim :")
-                # print(hit["_source"]["OMIMid"])
-                # print(hit["_source"]["OMIMdiseases"])
-        for orphaid in orpha : #my index has doublons so i take only the first result
-            resORPHA = es.search(index=INDEX_NAME3,body={"size":1,"query":{'match':{'ORPHAid':orphaid.split(":")[1]}}})
-            for hit in resORPHA["hits"]["hits"]:
-                diseases.append(hit["_source"]["diseases"])
-                # print("orpha :")
-                # print(hit["_source"]["ORPHAid"])
-                # print(hit["_source"]["diseases"])
-    return diseases
-#def createQueryEsearch(listSymptom)
-
-output = getORPHAandOMIMdiseasesfromSymptom(symptom1,symptom2)
-print(output)
-print(len(output))
+res = es.search(index=INDEX_NAME3,body={"query":{'match':{'ORPHAsymtpoms':"bleeding"}}})
+for hit in res["hits"]["hits"]:
+    print(hit["_source"]["ORPHAdisease"])
+    # print(hit["_source"]["ORPHAsymtpoms"])
